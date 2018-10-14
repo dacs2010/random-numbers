98ebade75334873ee6129a6fd67d1d03522e8488
Corn Buster <cornbuster@corns-air.home>
2018-10-14 09:55:55 -0400

    firts push

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+var result;
+function randomNumber(i) {
+  result = Math.round(Math.random() * i)
+  return
+}
+console.log(randomNumber(101));
+
+
+var numberOutput = document.querySelector('.print-out').textContent = result;
